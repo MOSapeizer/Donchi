4f2a03f0987acd84605cc6d061be26b5f56ebb47
unknown <>
2016-02-05 12:31:59 +0800

    Merge mos with Rspec capybara

--- Gemfile
+++ Gemfile
@@ -42,7 +42,6 @@ group :development, :test do
   gem 'byebug'
   gem "factory_girl_rails", "~> 4.0"
   gem 'rspec-rails', '~> 3.0'
-  gem 'factory_girl_rails'
   gem 'capybara'
 end
 
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,14 +4,10 @@ RSpec.describe "home page", type: :request do
 	it "show sign out after login" do
 		user = User.create!(email: "fuck@gmail.com", password: "password")
 		visit '/'
-
 		click_link 'sign in'
-
 		fill_in "user_email", with: "fuck@gmail.com"
 		fill_in "user_password", with: "password"
-
 		click_button "Log in"
-
 		expect(page).to have_selector(".login, .pull-right", :text => "sign out")
 	end
 end
