b3b70649b8ce839f02701f8cf13246a76136fc1f
unknown <>
2016-03-06 16:28:39 +0800

    add action sign_in

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,14 +1,34 @@
 class CourseController < ApplicationController
+	before_action :authenticate_user!, except: [:show]
 	def index
 		
 	end
 
 	def show
-		@course = Course.find(params[:id])
-		
+		if user_signed_in?
+			@user = current_user
+			@course = Course.find_by name: "course1"
+			@has = has_relation(@user,@course)
+
+		end
+
 	end
 
 	def create
 		
 	end
+
+
+	def sign_in
+		@user = current_user
+		@course = Course.find_by name:"course1"
+		@relation = RelationUserCourse.create(:user_id => @user.id , :course_id => @course.id)
+
+		redirect_to :action => "show" 
+	end
+
+	def has_relation(user,course)
+		RelationUserCourse.find_by(user_id: @user.id , course_id: @course.id).nil?
+
+	end	
 end
