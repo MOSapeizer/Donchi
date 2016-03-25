020b4ba47ca94d65fa243a33d73de5aa34acbc3e
unknown <>
2016-03-25 17:05:03 +0800

    Customize devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   root 'course#show'
 
-  devise_for :users, :skip => [:sessions]
+  devise_for :users, controllers: { registrations: "custom/registrations" }
 
   as :user do
     get 'login' => 'devise/sessions#new', :as => :new_user_login
     post 'login' => 'devise/sessions#create', :as => :user_login
     get 'signup' => 'devise/registrations#new', :as => :user_signup
-    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+    #delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
